c9ea3c4c252e352d580fc37677567fdf1af1f0e3
Abedaboutaleb <aboutalebabbe@gmail.com>
2018-03-13 11:00:31 +0100

    Adding setting.html and setting.js

---
+++ scripts/settings.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    // Get references to DOM elements
+  //  let $avatarPicker = $('ul#avatar-picker li');
+    //let $avatarPickerUrl = $('ul#avatar-picker li.active img');
+    let $settingsButton = $('#settings-button');
+    let $userName = $('#user-name');
+    let db = firebase.database();
+
+
+/*
+    // Set the chosen Avatar to active class.
+    $avatarPicker.click(function () {
+        $avatarPicker.removeClass('active');
+        $(this).addClass('active');
+    });
+*/
+
+    // Register new user.
+/*    $settingsButton.click(function () {
+
+        // Store Signup Details
+        let userName = $userName.val();
+        let userEmail = $userEmail.val();
+        let userPassword = $userPassword.val();
+      //  let userAvatar = $('ul#avatar-picker li.active img').attr('src');
+*/
+
+
+    $settingsButton.click(function () {
+        let userName = $userName.val();
+
+        // Update Profile.
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                 // User is signed in.
+                 var user = firebase.auth().currentUser;
+                  user.updateProfile({
+                      displayName: userName
+                    //  photoURL: userAvatar
+                  }).then(function () {
+                    location.reload();
+                      //window.location.href = "index.html";
+                      //console.log(user);
+                  });
+            }
+        });
+    });
+});
